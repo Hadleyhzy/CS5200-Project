f699e37d369d745bf18f4f405e148692fa01d6a7
Hadleyhzy <HOU.z@husky.neu.edu>
2015-12-05 21:51:41 -0500

    Update projectData.sql

--- projectData.sql
+++ projectData.sql
@@ -2,6 +2,6 @@ INSERT INTO Plate (name, area) VALUES
 ("African Plate", 61300000),
 ("Nazca Plate", 15600000);
 ("Pacific Plate",103300000);
-("Antarctic Plate"
+("Antarctic Plate",60900000);
 
 INSERT INTO Continent ()
