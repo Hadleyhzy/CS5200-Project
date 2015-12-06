8bb3abf7aee607ed6ab1428548b8fa367ad5aa8e
Hadleyhzy <HOU.z@husky.neu.edu>
2015-12-06 11:34:38 -0500

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,53 @@
+INSERT INTO Plate (name, area) VALUES
+("African Plate", 61300000);
+("Antarctic Plate",60900000);
+("Australia Plate",47000000);
+("Eurasian Plate",67800000);
+("Nazca Plate", 15600000);
+("North American Plate",75900000);
+("Pacific Plate",103300000);
+("Philippines Sea Plate",5555000);
+("Indian Plate",11900000);
+("South American Plate",43600000);
+("Somali Plate",16700000);
+("Cocos Plate",2900000);
+
+INSERT INTO Continent (name,area) VALUES
+("Africa",30221532);
+("Antartica",14000000);
+("Asia",44579000);
+("Australia Continent",8600000);
+("Europe",10180000);
+("North America",24709000);
+("South America",17840000);
+
+
+INSERT INTO RegionType (name,description) VALUES
+("river",);
+("lake",);
+("mountain",);
+("");
+
+INSERT INTO Region (name,area) VALUES
+
+INSERT INTO Location(latitude,longtitude,timezone) VALUES
+
+INSERT INTO Landscape(name, vegetation) VALUES
+
+INSERT INTO SurfaceElevation(min,max,ave) VALUES
+
+INSERT INTO Diversity(species) VALUES
+
+INSERT INTO ResourceType(name, description) VALUES
+
+INSERT INTO Resource(capacity) VALUES
+
+INSERT INTO ClimateType(name,description) VALUES
+
+INSERT INTO Climate(windspeed, humidity,precipitation,date) VALUES
+
+INSERT INTO Temperature(min,max,surfaceTemp,airTemp,averTemp) VALUES
+
+INSERT INTO DisasterType(name,description) VALUES
+
+INSERT INTO Disaster(start date time, end date time,casualty, propertylost)
